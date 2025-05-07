518a2827b951c4dec9fa33febc7c38ccef5d8b41
Federico Galvan <65232761+fedeGalvan@users.noreply.github.com>
2025-05-07 10:44:14 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen bg-black">
       <Link href={"/Game/minesweeper"}>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full shadow-lg transition ease-out transform hover:scale-105 text-md">
           Click to play
